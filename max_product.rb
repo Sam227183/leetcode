ce25d661d31432c55759bee5e69748507af2c3b3
John Doe <johndoe@email.com>
2025-05-12 19:14:49 -0600

    Solved Leetcode Q. 3536

---
+++ max_product.rb
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/maximum-product-of-two-digits/description/
+
+def max_product(n)
+  arr = n.to_s.split('').map(&:to_i).max(2)
+  return arr[0]*arr[1]
+end
+
+
+# Test Driven Development
+puts max_product(31)
+puts max_product(22)
+puts max_product(124)
+
