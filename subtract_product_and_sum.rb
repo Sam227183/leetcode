b851f207c120c0f2b8cbbeb1b54cfbbc31073791
John Doe <johndoe@email.com>
2025-05-13 16:32:19 -0600

    I added a shorter version.

--- subtract_product_and_sum.rb
+++ subtract_product_and_sum.rb
@@ -1,10 +1,20 @@
 # https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
 
+#Shorter version
+def subtract_product_and_sum(n)
+  arr = n.to_s.split('').map(&:to_i)
+  return arr.inject(:*) - arr.sum
+end
+
+#In this case, you are using above function only once.
+#Therefore, no need to store in variables.
+
+#Original version
 def subtract_product_and_sum(n)
   arr = n.to_s.split('').map(&:to_i)
   arr_product = arr.inject(:*)
   arr_sum = arr.sum
-  return arr_product - arr_sum
+  return arr_product - arr_sum  
 end
 
 # Test Driven Development
