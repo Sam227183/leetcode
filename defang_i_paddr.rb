61e183669fe444b4d4700a4761c2a5e176bc869a
John Doe <johndoe@email.com>
2025-06-03 17:38:07 -0600

    Solved Q. 1108

---
+++ defang_i_paddr.rb
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/defanging-an-ip-address/description/
+
+def defang_i_paddr(address)
+  puts address.gsub!(".","[.]")    
+end
+
+# Test Driven Development
+puts defang_i_paddr("1.1.1.1")
+puts defang_i_paddr("255.100.50.0")
