c609e3cdd283efeb05338d210ac480b670f32f5c
John Doe <johndoe@email.com>
2025-06-18 14:19:36 -0600

    Solved Q.1816

--- find_closest.rb
+++ find_closest.rb
@@ -1,19 +1,37 @@
 # https://leetcode.com/problems/find-closest-person/description/
 
+# def find_closest(x, y, z)
+#   subtract_xz = (x - z).abs
+#   subtract_yz = (y - z).abs
+
+#   if subtract_xz < subtract_yz
+#     "1"
+#   elsif subtract_xz > subtract_yz
+#     "2"
+#   elsif subtract_xz == subtract_yz
+#     "0"
+#   end  
+
+# end  
+
+
+# Test Driven Development
+# puts find_closest(2, 7, 4) 
+# puts find_closest(2, 5, 6) 
+# puts find_closest(1, 5, 3)
+
 def find_closest(x, y, z)
-  subtract_xz = (x - z).abs
-  subtract_yz = (y - z).abs
-
-  if subtract_xz < subtract_yz
-    "1"
-  elsif subtract_xz > subtract_yz
-    "2"
-  elsif subtract_xz == subtract_yz
-    "0"
-  end  
-
-end  
-
+  a = (x - z).abs
+  b = (y - z).abs
+  
+  if a > b
+    return 2
+  elsif a < b
+    return 1
+  elsif a = b
+    return 0
+  end
+end
 
 # Test Driven Development
 puts find_closest(2, 7, 4) 
