279881f391de03c6ee2339d264565f5167372bf1
John Doe <johndoe@email.com>
2025-06-23 17:23:37 -0600

    I solved for 2nd time.

--- power_of_two.rb
+++ power_of_two.rb
@@ -1,27 +1,45 @@
 # https://leetcode.com/problems/power-of-two/description/
 
-
-
 def is_power_of_two(n)
   if n.abs == 1 || n.abs == 2
     return true
   end
-end
 
-looping_var = n
+  looping_var = n
   
-while looping_var > 2
-  if looping_var % 2 != 0
-    return false
+  while looping_var > 2
+    if looping_var % 2 != 0 #odd number
+      return false
+    end
+    looping_var = looping_var / 2
   end
-  looping_var = looping_var / 2
-end
   return true
+end
 
+# #Test Driven Development
+# puts is_power_of_two(1) 
+# puts is_power_of_two(16) 
+# puts is_power_of_two(3)
+# puts is_power_of_two(20)
+# puts is_power_of_two(128)
 
-# Test Driven Development
-puts is_power_of_two(1) == true
-puts is_power_of_two(16) == true
-puts is_power_of_two(3) == false
-puts is_power_of_two(20) == false
-puts is_power_of_two(128) == true
+def is_power_of_two(n)
+  if n.abs == 1
+    return true
+  end
+
+  while n > 2
+    if n % 2 != 0
+      return false
+    end
+    n = n / 2
+  end
+  return true
+end
+
+#Test Driven Development
+puts is_power_of_two(1) 
+puts is_power_of_two(16) 
+puts is_power_of_two(3)
+puts is_power_of_two(20)
+puts is_power_of_two(128)
