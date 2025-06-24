c326f39a137a34a9f59b2951c9436cfa731feaec
John Doe <johndoe@email.com>
2025-06-24 14:00:28 -0600

    I sloved for 2nd time.

--- perfect_square.rb
+++ perfect_square.rb
@@ -17,6 +17,24 @@ end
 
 
 # Test Driven Development
-puts is_perfect_square(16) == true
-puts is_perfect_square(14) == false
+# puts is_perfect_square(16) == true
+# puts is_perfect_square(14) == false
 
+def is_perfect_square(num)
+  #1. edge case
+  return true if num == 1 
+
+  #2. loop
+  looping_var = 2
+
+  while looping_var < num
+    if (looping_var * looping_var) == num
+      return true
+    end
+    looping_var += 1
+  end
+  return false
+end
+
+puts is_perfect_square(16)
+puts is_perfect_square(14)
