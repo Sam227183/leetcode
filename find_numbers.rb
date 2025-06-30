8c782eed3f21d8d542538608d38931f14b8ad9bd
John Doe <johndoe@email.com>
2025-06-30 17:19:28 -0600

    Solved for 2nd time.

--- find_numbers.rb
+++ find_numbers.rb
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
 
+# Below answer is not correct. 
 def find_numbers(nums)
   # Generate an array
   
@@ -9,8 +10,26 @@ def find_numbers(nums)
 
 end
 
+# Test Driven Development
+puts find_numbers([12,345,2,6,7896])
+puts find_numbers([555,901,482,1771])
+
+# 2nd try. 
+def find_numbers(nums)
+  checker = 0
+  counter = 0
+
+  while checker < nums.length
+    if nums[checker].to_s.length % 2 == 0
+      counter += 1
+      checker += 1
+    else
+      checker += 1
+    end
+  end
+  return counter
+end
 
 # Test Driven Development
-puts find_numbers(12,345,2,6,7896)
-puts find_numbers(555,901,482,1771)
-
+puts find_numbers([12,345,2,6,7896])
+puts find_numbers([555,901,482,1771])
