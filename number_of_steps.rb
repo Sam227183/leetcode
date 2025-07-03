b2f266e952527139c4af1492da4e002653198b06
John Doe <johndoe@email.com>
2025-07-03 14:17:24 -0600

    2nd time solved.

--- number_of_steps.rb
+++ number_of_steps.rb
@@ -20,4 +20,26 @@ end
 # Test driven development
 puts number_of_steps(14)
 puts number_of_steps(8) 
-puts number_of_steps(123) 
+puts number_of_steps(123)
+
+# 2nd try
+
+def number_of_steps(num)
+   step_counter = 0
+
+   while num > 0
+      if num.even?
+         num = num / 2
+         step_counter += 1
+      elsif num.odd?
+         num = num - 1
+         step_counter += 1
+      end
+   end
+   return step_counter
+end
+
+# Test driven development
+puts number_of_steps(14)
+puts number_of_steps(8) 
+puts number_of_steps(123)
