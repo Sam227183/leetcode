fe4033b456ffa4e857657ac653b1de671399dc65
John Doe <johndoe@email.com>
2025-07-04 15:36:45 -0600

    Solved for 2nd time.

--- max_product.rb
+++ max_product.rb
@@ -5,9 +5,19 @@ def max_product(n)
   return arr[0]*arr[1]
 end
 
-
 # Test Driven Development
 puts max_product(31)
 puts max_product(22)
 puts max_product(124)
 
+# 2nd try
+
+def max_product(n)
+  selected = n.digits.max(2)
+  return selected.inject(:*)
+end
+
+# Test Driven Development
+puts max_product(31)
+puts max_product(22)
+puts max_product(124)
