779f89fcaae3cc9528f8d4a0460e0932117e2b70
John Doe <johndoe@email.com>
2025-07-14 17:39:53 -0600

    2nd time solved.

--- is_anagram.rb
+++ is_anagram.rb
@@ -7,3 +7,17 @@ end
 # Test Driven Development
 puts is_anagram('anagram', 'nagaram')
 puts is_anagram('rat', 'car')
+
+# 2nd try
+
+def is_anagram(s, t)
+  if t.split('').sort.join == s.split('').sort.join
+    return true
+  else 
+    return false
+  end
+end
+
+# Test Driven Development
+puts is_anagram('anagram', 'nagaram')
+puts is_anagram('rat', 'car')
