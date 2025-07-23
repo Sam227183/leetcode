a62fa1eb535acc1c328cb9d92fbfe517c5a793b0
John Doe <johndoe@email.com>
2025-07-23 16:30:45 -0600

    2nd time solved.

--- defang_i_paddr.rb
+++ defang_i_paddr.rb
@@ -7,3 +7,13 @@ end
 # Test Driven Development
 puts defang_i_paddr("1.1.1.1")
 puts defang_i_paddr("255.100.50.0")
+
+# 2nd try
+
+def defang_i_paddr(address)
+  return address.gsub!(".", "[.]")
+end
+
+# Test Driven Development
+puts defang_i_paddr("1.1.1.1")
+puts defang_i_paddr("255.100.50.0")
