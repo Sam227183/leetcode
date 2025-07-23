6c158a57558b5f39531c9371fbe008fcbcc5cb7d
John Doe <johndoe@email.com>
2025-07-23 16:37:57 -0600

    2nd time.

--- interpret.rb
+++ interpret.rb
@@ -7,3 +7,14 @@ end
 puts interpret("G()(al)")
 puts interpret("G()()()()(al)")
 puts interpret("(al)G(al)()()G") 
+
+# 2nd try
+
+def interpret(command)
+  return command.gsub!("()","o").gsub!("(al)","al")
+end
+
+# Test Driven Development
+puts interpret("G()(al)")
+puts interpret("G()()()()(al)")
+puts interpret("(al)G(al)()()G") 
